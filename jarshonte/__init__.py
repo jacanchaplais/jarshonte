d5dedfc28ae99855996873a732afac913fcfaa39
Jacan Chaplais <jlc1n20@soton.ac.uk>
2022-01-04 14:13:34 +0000

    added version printout

--- jarshonte/__init__.py
+++ jarshonte/__init__.py
@@ -1,6 +1,10 @@
+from setuptools_scm import get_version
 import click
 
+
+my_version = get_version()
 
 @click.command()
 def main():
     click.echo('Hello world!')
+    click.echo(f'This is version {my_version}.')
